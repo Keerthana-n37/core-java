1e5c198c3243310a2bf8617385f9cb2063d7b602
Keerthana-n37 <keerthana3739@gmail.com>
2026-03-11 09:11:50 +0530

    CarrotHalwa Example

---
+++ task20-Arrays/CarrotHalwa/Topping.java
@@ -0,0 +1,13 @@
+class Topping
+{
+    String name;
+    float weight;       
+    boolean isOptional;
+
+    Topping(String name, float weight, boolean isOptional)
+    {
+        this.name = name;
+        this.weight = weight;
+        this.isOptional = isOptional;
+    }
+}
