cd250a415ec118687091b24992d5384ebc98574a
Keerthana-n37 <keerthana3739@gmail.com>
2026-03-16 21:59:49 +0530

    Search in array

---
+++ SaveDataInArray/StoreScooterRunner.java
@@ -0,0 +1,36 @@
+class StoreScooterRunner
+{
+	public static void main(String[] args)
+	{
+		String[] scooterNames = new String[10];
+		StoreScooterNames storeScooter = new StoreScooterNames(scooterNames);
+		
+		storeScooter.getScooterName("Activa");
+		storeScooter.getScooterName("Jupiter");
+		storeScooter.getScooterName("Access");
+		storeScooter.getScooterName("Hero");
+		storeScooter.getScooterName("Pleasure");
+		storeScooter.getScooterName("Bajaj");
+		storeScooter.getScooterName("Suzuki");
+		storeScooter.getScooterName("VLF");
+		storeScooter.getScooterName("Ather");
+		storeScooter.getScooterName("Brisk");
+		System.out.println("-------------------");
+		
+		System.out.println("Search Found");
+        boolean found = storeScooter.searchScooterName("VLF");
+        System.out.println("Result: " + found);
+		System.out.println("-------------------");
+
+        System.out.println("Search Not Found");
+        boolean notFound = storeScooter.searchScooterName("TVS Scooty"); 
+        System.out.println("Result: " + notFound);
+		System.out.println("-------------------");
+
+        System.out.println("Search Null");
+        boolean nullCheck = storeScooter.searchScooterName(null);
+        System.out.println("Result: " + nullCheck);
+		
+		
+	}
+}
