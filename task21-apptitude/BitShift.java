4f99a8cd9f8355b686e5925ddd97a5460a6eb1f7
Keerthana-n37 <keerthana3739@gmail.com>
2026-03-27 21:29:14 +0530

    apptitude

---
+++ task21-apptitude/BitShift.java
@@ -0,0 +1,13 @@
+class BitShift
+{
+	static void shift()
+	{
+		int num=8;
+		
+		int leftShift = num<<1;
+		int rightShift = num>>1;
+		System.out.println("Original Number: "+num);
+		System.out.println("Left Shift: "+leftShift);
+		System.out.println("Right Shift: "+rightShift);
+	}
+}
