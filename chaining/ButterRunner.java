98a6701da3d5fc6a35cf2564f6762735d28e4fd1
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-01 20:51:27 +0530

    chaining

---
+++ chaining/ButterRunner.java
@@ -0,0 +1,17 @@
+class ButterRunner
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Butter 1 details");
+        DairyButter butter1 = new DairyButter("Creamy",200);
+        butter1.display();
+
+        System.out.println("---- Butter 2 ----");
+        DairyButter butter2 =new DairyButter("MilkyMist",65,"Unsalted","Classic",500);
+        butter2.display();
+
+        System.out.println("---- Butter 3 ----");
+        DairyButter butter3 =new DairyButter(55,"Salted","Garlic",250);
+        butter3.display();
+    }
+}
