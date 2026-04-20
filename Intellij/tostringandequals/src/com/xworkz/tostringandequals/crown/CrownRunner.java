2a5e99be22f12c84b3c5db5077ae924d5e6c947a
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-20 22:34:53 +0530

    override

--- Intellij/tostringandequals/src/com/xworkz/tostringandequals/crown/CrownRunner.java
+++ Intellij/tostringandequals/src/com/xworkz/tostringandequals/crown/CrownRunner.java
@@ -1,4 +1,17 @@
 package com.xworkz.tostringandequals.crown;
 
 public class CrownRunner {
+
+    public static void main(String[] args)
+    {
+        Crown crown1 = new Crown("Gold", 500, "Royal");
+        Crown crown2 = new Crown("Silver", 300, "Simple");
+        Crown crown3 = new Crown("Gold", 500, "Royal");
+
+        System.out.println(crown3);
+        System.out.println(crown2);
+        System.out.println(crown1);
+        boolean check = crown1.equals(crown3);
+        System.out.println("check = " + check);
+    }
 }
