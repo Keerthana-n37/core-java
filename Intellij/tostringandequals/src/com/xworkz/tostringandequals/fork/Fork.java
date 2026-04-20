d7b226c32c69ad5d5e78d0e46079894525232aa6
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-20 22:36:51 +0530

    override

--- Intellij/tostringandequals/src/com/xworkz/tostringandequals/fork/Fork.java
+++ Intellij/tostringandequals/src/com/xworkz/tostringandequals/fork/Fork.java
@@ -1,4 +1,38 @@
 package com.xworkz.tostringandequals.fork;
 
 public class Fork {
+
+    String materialType;
+    int size;
+    String color;
+
+    public Fork(String materialType, int size, String color)
+    {
+        this.materialType = materialType;
+        this.size = size;
+        this.color = color;
+    }
+
+    public String toString()
+    {
+        return "Material: " + materialType + ", Size: " + size + ", Color: " + color;
+    }
+
+    public boolean equals(Object obj)
+    {
+        Fork leftSide = this;
+
+        if(obj instanceof Fork)
+        {
+            Fork rightSide = (Fork)obj;
+
+            if(leftSide.materialType.equals(rightSide.materialType) &&
+                    leftSide.size == rightSide.size &&
+                    leftSide.color.equals(rightSide.color))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
