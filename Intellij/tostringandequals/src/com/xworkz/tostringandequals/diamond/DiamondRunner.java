4f799f68a6b641814716ed93e0161e263368985f
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-20 22:36:05 +0530

    override

--- Intellij/tostringandequals/src/com/xworkz/tostringandequals/diamond/DiamondRunner.java
+++ Intellij/tostringandequals/src/com/xworkz/tostringandequals/diamond/DiamondRunner.java
@@ -1,4 +1,17 @@
 package com.xworkz.tostringandequals.diamond;
 
 public class DiamondRunner {
+    public static void main(String[] args)
+    {
+        Diamond diamond1 = new Diamond("White", 1.5, 50000);
+        Diamond diamond2 = new Diamond("Yellow", 2.0, 60000);
+        Diamond diamond3 = new Diamond("White", 1.7, 50000);
+
+        System.out.println(diamond1);
+        System.out.println(diamond2);
+        System.out.println(diamond3);
+
+        boolean check = diamond1.equals(diamond3);
+        System.out.println("check = " + check);
+    }
 }
