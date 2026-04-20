65b7149c271060e21e9abddc01b1e1094fe2d42a
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-20 22:38:38 +0530

    override

--- Intellij/tostringandequals/src/com/xworkz/tostringandequals/industry/IndustryRunner.java
+++ Intellij/tostringandequals/src/com/xworkz/tostringandequals/industry/IndustryRunner.java
@@ -1,4 +1,17 @@
 package com.xworkz.tostringandequals.industry;
 
 public class IndustryRunner {
+
+    public static void main(String[] args)
+    {
+        Industry industry1 = new Industry("Infosys", "Bangalore", 1000);
+        Industry industry2 = new Industry("TCS", "Hyderabad", 2000);
+        Industry industry3 = new Industry("Infosys", "Bangalore", 1000);
+
+        System.out.println(industry1);
+        System.out.println(industry2);
+        System.out.println(industry3);
+        boolean check = industry1.equals(industry3);
+        System.out.println("check = " + check);
+    }
 }
