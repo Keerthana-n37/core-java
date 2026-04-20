7f2c764b809573f28e4898591aee62a7f7cc597c
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-20 22:38:21 +0530

    override

--- Intellij/tostringandequals/src/com/xworkz/tostringandequals/industry/Industry.java
+++ Intellij/tostringandequals/src/com/xworkz/tostringandequals/industry/Industry.java
@@ -1,4 +1,38 @@
 package com.xworkz.tostringandequals.industry;
 
 public class Industry {
+
+    String industryName;
+    String place;
+    int employeeCount;
+
+    public Industry(String industryName, String place, int employeeCount)
+    {
+        this.industryName = industryName;
+        this.place = place;
+        this.employeeCount = employeeCount;
+    }
+
+    public String toString()
+    {
+        return "Name: " + industryName + ", Place: " + place + ", Employees: " + employeeCount;
+    }
+
+    public boolean equals(Object obj)
+    {
+        Industry leftSide = this;
+
+        if(obj instanceof Industry)
+        {
+            Industry rightSide = (Industry)obj;
+
+            if(leftSide.industryName.equals(rightSide.industryName) &&
+                    leftSide.place.equals(rightSide.place) &&
+                    leftSide.employeeCount == rightSide.employeeCount)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
