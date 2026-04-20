37db7879550b7a92843e65a829c52126ee9acf3b
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-20 22:34:08 +0530

    override

--- Intellij/tostringandequals/src/com/xworkz/tostringandequals/crown/Crown.java
+++ Intellij/tostringandequals/src/com/xworkz/tostringandequals/crown/Crown.java
@@ -1,4 +1,38 @@
 package com.xworkz.tostringandequals.crown;
 
 public class Crown {
+
+    String metalType;
+    int weight;
+    String style;
+
+    public Crown(String metalType, int weight, String style)
+    {
+        this.metalType = metalType;
+        this.weight = weight;
+        this.style = style;
+    }
+
+    public String toString()
+    {
+        return "Metal: " + metalType + ", Weight: " + weight + ", Style: " + style;
+    }
+
+    public boolean equals(Object obj)
+    {
+        Crown leftSide = this;
+
+        if(obj instanceof Crown)
+        {
+            Crown rightSide = (Crown)obj;
+
+            if(leftSide.metalType.equals(rightSide.metalType) &&
+                    leftSide.weight == rightSide.weight &&
+                    leftSide.style.equals(rightSide.style))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
