905a74db1cc0b628ab22d67a9da30c49f3294a90
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-20 22:37:23 +0530

    override

--- Intellij/tostringandequals/src/com/xworkz/tostringandequals/fork/ForkRunner.java
+++ Intellij/tostringandequals/src/com/xworkz/tostringandequals/fork/ForkRunner.java
@@ -1,4 +1,16 @@
 package com.xworkz.tostringandequals.fork;
 
 public class ForkRunner {
+    public static void main(String[] args)
+    {
+        Fork fork1 = new Fork("Steel", 8, "Silver");
+        Fork fork2 = new Fork("Plastic", 6, "White");
+        Fork fork3 = new Fork("Steel", 8, "Silver");
+
+        System.out.println(fork1);
+        System.out.println(fork2);
+        System.out.println(fork3);
+        boolean check = fork1.equals(fork3);
+        System.out.println("check = " + check);
+    }
 }
