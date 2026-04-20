95954f0a629729dd67f84baf315e69743ba516ec
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-20 22:33:08 +0530

    override

--- Intellij/tostringandequals/src/com/xworkz/tostringandequals/battery/BatteryRunner.java
+++ Intellij/tostringandequals/src/com/xworkz/tostringandequals/battery/BatteryRunner.java
@@ -1,4 +1,17 @@
 package com.xworkz.tostringandequals.battery;
 
 public class BatteryRunner {
+
+    public static void main(String[] args)
+    {
+        Battery battery1 = new Battery("Lithium", 5000, "Duracell");
+        Battery battery2 = new Battery("Lead", 3000, "Exide");
+        Battery battery3 = new Battery("Lithium", 6000, "Duracell");
+
+        System.out.println(battery1);
+        System.out.println(battery2);
+        System.out.println(battery3);
+        boolean check = battery1.equals(battery3);
+        System.out.println("check = " + check);
+    }
 }
