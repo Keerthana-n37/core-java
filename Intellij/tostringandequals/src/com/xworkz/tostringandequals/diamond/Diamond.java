7b53117b710fbcdde5555ddb428590ebd5f5440a
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-20 22:35:38 +0530

    override

--- Intellij/tostringandequals/src/com/xworkz/tostringandequals/diamond/Diamond.java
+++ Intellij/tostringandequals/src/com/xworkz/tostringandequals/diamond/Diamond.java
@@ -1,4 +1,38 @@
 package com.xworkz.tostringandequals.diamond;
 
 public class Diamond {
+
+    String colorType;
+    double weight;
+    int price;
+
+    public Diamond(String colorType, double weight, int price)
+    {
+        this.colorType = colorType;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public String toString()
+    {
+        return "Color: " + colorType + ", Weight: " + weight + ", Price: " + price;
+    }
+
+    public boolean equals(Object obj)
+    {
+        Diamond leftSide = this;
+
+        if(obj instanceof Diamond)
+        {
+            Diamond rightSide = (Diamond)obj;
+
+            if(leftSide.colorType.equals(rightSide.colorType) &&
+                    leftSide.weight == rightSide.weight &&
+                    leftSide.price == rightSide.price)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
